53bc0934185496728ae6a7457d1e6c9b58996014
Kash Pitan <kashpitan@googlemail.com>
2021-01-17 17:49:29 +0000

    Changed frontend to use env variables for api calls Changed auth server to allows CORS

--- client/src/pages/MyProfile.js
+++ client/src/pages/MyProfile.js
@@ -3,8 +3,24 @@ import axios from "axios";
 
 const MyProfile = () => {
   useEffect(() => {
-    axios.get("/me");
+    console.log(`${process.env.REACT_APP_AUTH_API_URL}/profile/me`);
+    console.log(
+      axios.get(`${process.env.REACT_APP_AUTH_API_URL}/profile/me`, {
+        headers: {
+          Authorization: "Bearer ",
+        },
+      })
+    );
   }, []);
-  return <div></div>;
+
+  return (
+    <>
+      <div className="container">
+        <h1>
+          <strong>My Profile</strong>
+        </h1>
+      </div>
+    </>
+  );
 };
 export default MyProfile;
