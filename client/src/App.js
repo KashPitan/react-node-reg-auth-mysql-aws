bd179ab74ee00a5bbfd28904e682bc86e6e8e1ce
Kash Pitan <kashpitan@googlemail.com>
2021-05-24 00:46:34 +0100

    feat: added new router component

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,12 @@
 import React from "react";
 import "./App.css";
-import Registration from "./pages/Registration";
-import Login from "./pages/Login";
-import MyProfile from "./pages/MyProfile";
-import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Routes from "./routes";
 
 const App = () => {
   return (
     <>
-      <Navbar></Navbar>
-      <Router>
-        <Switch>
-          <Route path="/myProfile">
-            <MyProfile />
-          </Route>
-          <Route path="/register">
-            <Registration />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/">
-            <Login />
-          </Route>
-        </Switch>
-      </Router>
+      <Routes></Routes>
     </>
   );
 };
